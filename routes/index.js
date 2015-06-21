2dfe0d79a8e71b63ccda76e7e5c99ca254ebf97e
Felix Chinea <tabai.net@gmail.com>
2015-06-21 20:13:27 +0100

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET Credits page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
