9c65c7981bbcd02ba5179ff3cbf478638f42978a
Felix Chinea <tabai.net@gmail.com>
2015-06-22 19:10:31 +0100

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,9 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
